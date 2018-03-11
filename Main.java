d8f34eed82efc9305ff5f9ab8e736b44ea5954f6
Yoshi <crispy245@gmail.com>
2018-03-11 21:24:38 +0900

    First commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+import com.yoshi_self.Base32Decoder;
+
+/**
+ * Example usage of Base32Decoder
+ */
+public class Main {
+    public static void main(String args[]) {
+        // encoded: "This is Base32Decoder"
+        String encodedString = "KRUGS4ZANFZSAQTBONSTGMSEMVRW6ZDFOI======";
+        Charset charset = StandardCharsets.UTF_8;
+        Base32Decoder decoder = new Base32Decoder(encodedString.getBytes(charset));
+        byte[] decodedBytes = decoder.decode();
+        String result = new String(decodedBytes, charset);
+        System.out.println(result);
+    }
+}
